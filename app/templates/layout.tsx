d2a35c779dd949dcae90459a589c7cb132c22cd0
seattledesitv <seattledesitv@gmail.com>
2026-08-27 18:40:48 -0700

    Add templates layout

---
+++ app/templates/layout.tsx
@@ -0,0 +1,7 @@
+import AppShell from "@/app/components/AppShell";
+import { requireApprovedUser } from "@/lib/access";
+
+export default async function TemplatesLayout({ children }: { children: React.ReactNode }) {
+  const { access } = await requireApprovedUser();
+  return <AppShell isSuperAdmin={access.role === "super_admin"}>{children}</AppShell>;
+}
